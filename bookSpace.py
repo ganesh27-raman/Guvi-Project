e6c0d30ec4f484404e4f59ea525d65afd236c325
ganesh27-raman <80832436+ganesh27-raman@users.noreply.github.com>
2024-12-20 19:23:29 +0530

    Update bookSpace.py

--- bookSpace.py
+++ bookSpace.py
@@ -5,7 +5,7 @@ import streamlit as st
 def get_books(search_title=None, search_author=None, search_category=None):
 
     # Connect to MySQL database
-    connection = st.connection("sql")
+    connection = st.connection('mysql', type='sql')
     # Create a cursor object to interact with the database
     cursor = connection.cursor()
 
